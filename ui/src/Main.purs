9045a4a485e94ae49cb03a8d7904802f8c25e996
Matt Parsons <parsonsmatt@gmail.com>
2016-11-08 11:52:28 -0700

    Clean up warnings

--- ui/src/Main.purs
+++ ui/src/Main.purs
@@ -2,42 +2,28 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Aff
-import Control.Monad.Except.Trans
-import Control.Monad.Reader.Trans
-import PureFlowy.Api
-import Data.Argonaut.Generic.Aeson
-import Data.Either
-import Data.Generic
-import Data.Maybe
-import Servant.PureScript.Affjax
-import Servant.PureScript.Settings
-import PureFlowy.Api.MakeRequests as MakeReq
+import Control.Monad.Aff (Aff)
+import Control.Monad.Except.Trans (ExceptT, runExceptT)
+import Control.Monad.Reader.Trans (ReaderT, runReaderT)
+import Data.Argonaut.Generic.Aeson (decodeJson, encodeJson)
+import Data.Either (Either(..))
+import Data.Generic (gShow)
+import Servant.PureScript.Affjax (AjaxError)
+import Servant.PureScript.Settings (SPSettings_(..))
 import Data.Array as Array
 import Control.Bind ((<=<))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Control.Monad.Eff.Ref (REF)
-import DOM.Node.Node (baseURI)
-import Data.Argonaut.Core (Json)
-import Data.Argonaut.Parser (jsonParser)
-import Data.Bifunctor (lmap)
-import Data.Foldable (foldr, fold)
-import Data.List (List(Nil, Cons))
-import Data.Tuple (Tuple(Tuple))
-import Network.HTTP.Affjax (AJAX, get)
-import Pux (renderToDOM, fromSimple, start, EffModel, noEffects)
-import Signal (Signal)
-import Signal.Channel (Channel, subscribe, send, channel, CHANNEL)
-import Unsafe.Coerce (unsafeCoerce)
-import WebSocket (WEBSOCKET)
+import Network.HTTP.Affjax (AJAX)
+import Pux (renderToDOM, start, EffModel, noEffects)
+import Signal.Channel (CHANNEL)
 
-import PureFlowy.Api as Api
-import PureFlowy.Api.Todos
-import PureFlowy.State
-import PureFlowy.View
-import PureFlowy.Action
-import PureFlowy.Settings
+import PureFlowy.Api (getTodos, postTodos, deleteTodosById, SPParams_(..))
+import PureFlowy.Api.Todos (Todo(..))
+import PureFlowy.State (State, initialState)
+import PureFlowy.View (view)
+import PureFlowy.Action (Action(..))
+import PureFlowy.Settings (MySettings)
 
 type App eff
     = ReaderT MySettings (ExceptT AjaxError (Aff (Effects eff)))
@@ -51,12 +37,12 @@ update (ReportError _) state =
     noEffects state
 update GetTodos state =
     runEffectActions state
-        [ LoadTodos <$> Api.getTodos "" -- Should be a Maybe
+        [ LoadTodos <$> getTodos "" -- Should be a Maybe
         ]
 update (LoadTodos newTodos) state =
     noEffects state { todos = newTodos }
 update (ModifyCurrentTodo ev) state =
-    noEffects state { currentTodo =  ev.target.value }
+    noEffects state { currentTodo = ev.target.value }
 update CreateTodo state =
     runEffectActions (state { currentTodo = "" })
         [ do
@@ -73,10 +59,7 @@ update (DeleteTodo id) state =
         [ Nop <$ deleteTodosById id
         ]
   where
-    p (Todo { todoId }) = todoId /= id
-
-
-
+    p (Todo r) = r.todoId /= id
 
 runEffectActions :: State -> Array (App () Action) -> EffModel State Action (ajax :: AJAX)
 runEffectActions state effects =
@@ -95,7 +78,7 @@ runEffect settings m = do
       Left err -> pure $ ReportError err
       Right v -> pure v
 
-main :: forall eff. Eff (ajax :: AJAX, err :: EXCEPTION, ref :: REF | eff) Unit
+main :: Eff (channel :: CHANNEL, err :: EXCEPTION, ajax :: AJAX) Unit
 main = do
   let settings = SPSettings_ {
                     encodeJson : encodeJson
@@ -103,7 +86,7 @@ main = do
                   , toURLPiece : gShow
                   , params : SPParams_ { baseURL : "http://localhost:8081/" }
                   }
-  app <- coerceEffects <<< start $
+  app <- start $
     { initialState: initialState settings
     , update: update
     , view: view
@@ -111,6 +94,3 @@ main = do
     }
 
   renderToDOM "#app" app.html
-
-coerceEffects :: forall eff0 eff1 a. Eff eff0 a -> Eff eff1 a
-coerceEffects = unsafeCoerce
